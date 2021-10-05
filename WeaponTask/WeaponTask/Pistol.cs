3a11a14e55424d3a3470dc425024b6f83f841ad7
unknown <you@example.com>
2021-10-05 11:51:19 +0400

    task

---
+++ WeaponTask/WeaponTask/Pistol.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon
+{
+    class Rifle
+    {
+        public int MaxAmmo;
+        public int UAmmo;
+        private int _capacity;
+        public Rifle()
+        {
+
+        }
+        public Rifle(int cap, int maxAmmo, int uAmmo)
+        {
+            _capacity = cap;
+            MaxAmmo = maxAmmo;
+            UAmmo = uAmmo;
+        }
+        public int Daraq
+        {
+            get
+            {
+                if (_capacity == 0)
+                {
+                    return -1;
+                }
+                return _capacity;
+            }
+            set
+            {
+                if (value < 10 || value > 40)
+                {
+                    Console.WriteLine("10 ile 40 arasi olmalidir");
+                    return;
+                }
+                _capacity = value;
+            }
+        }
+
+        public void Shot()
+        {
+            for (int i = 0; i < MaxAmmo; i++)
+            {
+                if (i == Daraq && Daraq > UAmmo)
+                {
+                    MaxAmmo -= Daraq - (Daraq - UAmmo);
+                }
+                if (MaxAmmo < Daraq)
+                {
+                    Console.WriteLine("Yeterince gulleniz yoxdur");
+                    break;
+                }if (MaxAmmo < Daraq)
+                {
+                    Console.WriteLine("Daraq tutumdan Boyuk Ola bilmez");
+                    break;
+
+                }
+            }
+
+            Console.WriteLine($"Gulleniz ne qederdi : {MaxAmmo - UAmmo + Daraq - UAmmo}");
+        }
+        public void Reload()
+        {
+            if (Daraq >= UAmmo)
+            {
+                if (Daraq == UAmmo)
+                {
+                    Console.WriteLine("Yenilendi");
+                }
+                else
+                    Console.WriteLine($"Daraqda {Daraq - UAmmo} gulle qalib");
+            }
+            else Console.WriteLine("Tutum,istifade olunacaq gulle sayindan az ola bilmez");
+            
+        }
+    }
+}
---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Weapon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rifle rf1 = new Rifle();
+            Console.WriteLine("Gulle sayi:");
+            int T_ammo = int.Parse(Console.ReadLine());
+            rf1.MaxAmmo = T_ammo;
+            Console.WriteLine("Tutum:");
+            int cap = int.Parse(Console.ReadLine());
+            rf1.Daraq = cap;
+
+            if (T_ammo >= cap)
+            {
+                Console.WriteLine("Istifade olunacaq gulle sayi:");
+                int usedammo = int.Parse(Console.ReadLine());
+                rf1.UAmmo = usedammo;
+                rf1.Reload();
+            }
+            rf1.Shot();
+        }
+    }
+}
